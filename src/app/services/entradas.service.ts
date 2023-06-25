f414dfbad312c47b63f211a4a431ad0a0f109961
CLAUDIA HERNANDEZ <claudiahergre@gmail.com>
2023-06-25 19:01:23 +0200

    metodos crear entrada y getall

--- src/app/services/entradas.service.ts
+++ src/app/services/entradas.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Entrada } from '../interfaces/entradas.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EntradasService {
 
-  constructor() { }
+  private arrEntradas: Entrada[]
+
+  constructor() {
+    this.arrEntradas = [
+      {
+        date: '',
+        tittle: '',
+        description: '',
+      }
+    ]
+  }
+
+  getAll(): Entrada[] {
+    return this.arrEntradas;
+  }
+
+
+  crearEntrada(nuevaEntrada: Entrada) {
+    this.arrEntradas.push(nuevaEntrada)
+    console.log(this.arrEntradas)
+  }
+
+
 }
